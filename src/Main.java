4bcd680c34c9a3f97ca8f6c1fb29b420b1e79859
Michael <obidos.michael.cautiber@gmail.com>
2024-10-22 19:32:05 +0800

    modify the Main class and adding a new line

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main{
     public static void main(String[] args){
         System.out.println("Hello Git!");
         System.out.println("Hi Git!");
-        System.out.println("adding this in feature-branch");
+        System.out.println("Welcome to Git");
+        System.out.println("this is coming from dev-adrian");
     }
 }
